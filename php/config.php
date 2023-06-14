fc8eac2235fa6bfcdd1a3f9159a5e7e0eb476f7c
mwaustephen <stephenmwau41@gmail.com>
2023-06-14 13:11:22 +0300

    changed the config file

--- php/config.php
+++ php/config.php
@@ -1,11 +1,17 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "root";
-  $dbname = "chatapp";
+  // store db details into variables
+$db['db_user']="localhost";
+$db['db_username']="root";
+$db['db_pass']="root";
+$db['db_name']="chatapp";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
-  if(!$conn){
-    echo "Database connection error".mysqli_connect_error();
-  }
+// convert the variables into constants and uppercase
+
+foreach($db as $key => $value){
+    // creating constants
+    define(strtoupper($key), $value);
+}
+
+$connect=mysqli_connect(DB_USER,DB_USERNAME,DB_PASS,DB_NAME);
+
 ?>
