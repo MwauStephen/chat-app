87d7eee1e2eae4e83e45a53f615d4c20d9fcd61c
mwaustephen <stephenmwau41@gmail.com>
2023-06-14 12:52:05 +0300

    added some comments

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,27 +1,28 @@
 const form = document.querySelector(".signup form"),
-continueBtn = form.querySelector(".button input"),
-errorText = form.querySelector(".error-text");
+  continueBtn = form.querySelector(".button input"),
+  errorText = form.querySelector(".error-text");
 
-form.onsubmit = (e)=>{
-    e.preventDefault();
-}
+form.onsubmit = (e) => {
+  e.preventDefault();
+};
 
-continueBtn.onclick = ()=>{
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/signup.php", true);
-    xhr.onload = ()=>{
-      if(xhr.readyState === XMLHttpRequest.DONE){
-          if(xhr.status === 200){
-              let data = xhr.response;
-              if(data === "success"){
-                location.href="users.php";
-              }else{
-                errorText.style.display = "block";
-                errorText.textContent = data;
-              }
-          }
+continueBtn.onclick = () => {
+  let xhr = new XMLHttpRequest();
+  // opens a post request and sends form content to php/signup.php
+  xhr.open("POST", "php/signup.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        if (data === "success") {
+          location.href = "users.php";
+        } else {
+          errorText.style.display = "block";
+          errorText.textContent = data;
+        }
       }
     }
-    let formData = new FormData(form);
-    xhr.send(formData);
-}
+  };
+  let formData = new FormData(form);
+  xhr.send(formData);
+};
