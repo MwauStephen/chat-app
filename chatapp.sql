be951879b7c0c73984f843a11de93ef30ebb3d46
mwaustephen <stephenmwau41@gmail.com>
2023-06-16 14:23:59 +0300

    added the sql query file

--- chatapp.sql
+++ chatapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 18, 2021 at 11:44 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.26
+-- Host: localhost:3306
+-- Generation Time: Jun 16, 2023 at 11:11 AM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,6 +34,19 @@ CREATE TABLE `messages` (
   `msg` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES
+(1, 386233390, 953708271, 'hello alex'),
+(2, 386233390, 953708271, 'i meant hello steve'),
+(3, 953708271, 386233390, 'hello alex'),
+(4, 386233390, 953708271, 'aszfdcxvb'),
+(5, 953708271, 386233390, 'aershgfnb'),
+(6, 953708271, 910386752, 'hello'),
+(7, 910386752, 953708271, 'you good');
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +63,15 @@ CREATE TABLE `users` (
   `img` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`) VALUES
+(1, 386233390, 'Stephen', 'Mwau', 'kylestevens2@gmail.com', '202cb962ac59075b964b07152d234b70', '1686738471crop.jpg', 'Active now'),
+(2, 953708271, 'Alex', 'Karanja', 'karanja21@gmail.com', '2be9bd7a3434f7038ca27d1918de58bd', '168673901697.jpg', 'Offline now'),
+(3, 910386752, 'Tony', 'Blake', 'blake21@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '1686740557tn.jpg', 'Offline now');
 
 --
 -- Indexes for dumped tables
@@ -75,13 +97,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
